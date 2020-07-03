041eed15029afdb638a41f78fe463d5d18794100
Siva Mahadevan <me@svmhdvn.name>
2020-07-03 15:25:07 -0400

    [WIP] aerc: add module

---
+++ modules/programs/aerc-accounts.nix
@@ -0,0 +1,42 @@
+{ config, lib, ... }:
+
+with lib;
+
+{
+  options.aerc = {
+    enable = mkEnableOption "Aerc";
+
+    source = mkOption {
+      type = types.enum [ "imap", "maildir" ];
+      description = "Source for reading incoming emails.";
+    };
+
+    sendMailCommand = mkOption {
+      type = types.nullOr types.str;
+      default = if config.msmtp.enable then
+        "msmtpq --read-envelope-from --read-recipients"
+      else
+        null;
+      defaultText = literalExample ''
+        if config.msmtp.enable then
+          "msmtpq --read-envelope-from --read-recipients"
+        else
+          null
+      '';
+      example = "msmtpq --read-envelope-from --read-recipients";
+      description = ''
+        Command to send a mail. If not set, aerc will be in charge of sending mails.
+      '';
+    };
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      example = literalExample ''
+        archive = Archive
+        folders-sort = Archive,Sent
+      '';
+      description = "Extra lines to add to this account's specific configuration.";
+    };
+  };
+}
