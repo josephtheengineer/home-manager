da104a5599ddac1d18e4035284866719fa612ea2
josephtheengineer <joseph@theengineer.life>
2020-07-15 21:01:03 +1000

    move aerc patch to overlay.nix

--- overlay.nix
+++ overlay.nix
@@ -1,3 +1,11 @@
 self: super: {
   home-manager = super.callPackage ./home-manager { path = toString ./.; };
+
+  # Add patch to ignore aerc accounts file permissions
+  aerc = super.aerc.override {
+    patches = [
+      ./runtime-sharedir.patch
+      ./do-not-fail-on-open-permissions.patch
+    ];
+  };
 }
